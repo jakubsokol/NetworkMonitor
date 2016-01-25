7a61bf33c8cc70a798d9192162977dfafc9ae732
Kabun <raal98@tlen.pl>
2016-01-25 11:39:20 +0100

    add neighbor cells info

--- app/src/main/java/com/skru/networkmonitor/MainActivity.java
+++ app/src/main/java/com/skru/networkmonitor/MainActivity.java
@@ -2,7 +2,6 @@ package com.skru.networkmonitor;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.telephony.CellIdentityCdma;
 import android.telephony.CellIdentityGsm;
 import android.telephony.CellIdentityLte;
 import android.telephony.CellIdentityWcdma;
@@ -11,9 +10,7 @@ import android.telephony.CellInfoCdma;
 import android.telephony.CellInfoGsm;
 import android.telephony.CellInfoLte;
 import android.telephony.CellInfoWcdma;
-import android.telephony.CellSignalStrengthCdma;
 import android.telephony.CellSignalStrengthGsm;
-import android.telephony.CellSignalStrengthLte;
 import android.telephony.CellSignalStrengthWcdma;
 import android.telephony.PhoneStateListener;
 import android.telephony.SignalStrength;
@@ -43,6 +40,32 @@ public class MainActivity extends Activity {
     TextView                        tac_value;
     TextView                        neighbor1_network_type_value;
     TextView                        pci_name, tac_name, ci_name;
+
+    TextView                        rsrq_name1;
+    TextView                        rssnr_name1;
+    TextView                        rsrp_name1;
+    TextView                        rsrp_value1;
+    TextView                        rsrq_value1;
+    TextView                        rssnr_value1;
+    TextView                        asu_value1;
+    TextView                        power_value1;
+    TextView                        ci_value1;
+    TextView                        pci_value1;
+    TextView                        tac_value1;
+    TextView                        pci_name1, tac_name1, ci_name1;
+
+    TextView                        rsrq_name2;
+    TextView                        rssnr_name2;
+    TextView                        rsrp_name2;
+    TextView                        rsrp_value2;
+    TextView                        rsrq_value2;
+    TextView                        rssnr_value2;
+    TextView                        asu_value2;
+    TextView                        power_value2;
+    TextView                        ci_value2;
+    TextView                        pci_value2;
+    TextView                        tac_value2;
+    TextView                        pci_name2, tac_name2, ci_name2;
 
     SignalPhoneStateListener        signal_listener;
     NetworkPhoneStateListener       network_listener;
@@ -87,6 +110,36 @@ public class MainActivity extends Activity {
         pci_name                     = (TextView)findViewById(R.id.pci_name);
         rsrq_name                    = (TextView)findViewById(R.id.rsrq_name);
         rssnr_name                   = (TextView)findViewById(R.id.rssnr_name);
+
+        rsrp_value1                   = (TextView) findViewById(R.id.rsrp_value1);
+        rsrq_value1                   = (TextView) findViewById(R.id.rsrq_value1);
+        rssnr_value1                  = (TextView) findViewById(R.id.rssnr_value1);
+        asu_value1                    = (TextView) findViewById(R.id.asu_value1);
+        power_value1                  = (TextView) findViewById(R.id.power_value1);
+        ci_value1                     = (TextView) findViewById(R.id.ci_value1);
+        pci_value1                    = (TextView) findViewById(R.id.pci_value1);
+        tac_value1                    = (TextView) findViewById(R.id.tac_value1);
+        rsrp_name1                    = (TextView)findViewById(R.id.rsrp_name1);
+        ci_name1                      = (TextView)findViewById(R.id.ci_name1);
+        tac_name1                     = (TextView)findViewById(R.id.tac_name1);
+        pci_name1                     = (TextView)findViewById(R.id.pci_name1);
+        rsrq_name1                    = (TextView)findViewById(R.id.rsrq_name1);
+        rssnr_name1                   = (TextView)findViewById(R.id.rssnr_name1);
+
+        rsrp_value2                   = (TextView) findViewById(R.id.rsrp_value2);
+        rsrq_value2                   = (TextView) findViewById(R.id.rsrq_value2);
+        rssnr_value2                  = (TextView) findViewById(R.id.rssnr_value2);
+        asu_value2                    = (TextView) findViewById(R.id.asu_value2);
+        power_value2                  = (TextView) findViewById(R.id.power_value2);
+        ci_value2                     = (TextView) findViewById(R.id.ci_value2);
+        pci_value2                    = (TextView) findViewById(R.id.pci_value2);
+        tac_value2                    = (TextView) findViewById(R.id.tac_value2);
+        rsrp_name2                    = (TextView)findViewById(R.id.rsrp_name2);
+        ci_name2                      = (TextView)findViewById(R.id.ci_name2);
+        tac_name2                     = (TextView)findViewById(R.id.tac_name2);
+        pci_name2                     = (TextView)findViewById(R.id.pci_name2);
+        rsrq_name2                    = (TextView)findViewById(R.id.rsrq_name2);
+        rssnr_name2                   = (TextView)findViewById(R.id.rssnr_name2);
 
         signal_listener              = new SignalPhoneStateListener();
         network_listener             = new NetworkPhoneStateListener();
@@ -169,7 +222,7 @@ public class MainActivity extends Activity {
             } else if (telephon_manager_1.getNetworkType() == TelephonyManager.NETWORK_TYPE_HSPAP ||
                     telephon_manager_1.getNetworkType() == TelephonyManager.NETWORK_TYPE_HSPA) {
                 Log.i("TAG ", " HSPAP/HAPA");
-                signalStrengthValue = signalStrength.getGsmSignalStrength() * 2 - 113;
+                signalStrengthValue = signalStrength.getGsmSignalStrength() * 2 - 113;  // dlaczego z sygnału GSM bierzemy sile sygnalu?
 
                 cellInfoList = telephon_manager_1.getAllCellInfo();
                 if (cellInfoList != null && (cellInfoList.get(0) instanceof CellInfoWcdma)) {
@@ -177,7 +230,7 @@ public class MainActivity extends Activity {
                     cellIdentityWcdma = cellInfoWcdma.getCellIdentity();
 
                     cellSignalStrengthWcdma = cellInfoWcdma.getCellSignalStrength();
-
+                    signalStrengthValue  = cellSignalStrengthWcdma.getDbm();
                     rsrp_name.setText("RSSI:");
                     rsrq_name.setText("");
                     rssnr_name.setText("");
@@ -186,7 +239,7 @@ public class MainActivity extends Activity {
                     ci_name.setText("CID:");
 
                     rsrp_value.setText(String.valueOf(signalStrengthValue) + " dbm");
-                    asu_value.setText(String.valueOf(signalStrengthValue + 116));
+                    asu_value.setText(String.valueOf(cellSignalStrengthWcdma.getAsuLevel()));
                     power_value.setText(dbmToPower(signalStrengthValue));
                     rsrq_value.setText("");
                     rssnr_value.setText("");
@@ -195,6 +248,67 @@ public class MainActivity extends Activity {
                     pci_value.setText(Integer.toString(cellIdentityWcdma.getPsc()));
                     ci_value.setText(Integer.toString(cellIdentityWcdma.getCid()));
                 }
+                int number_of_cells = telephon_manager_1.getNeighboringCellInfo().size();
+                if(number_of_cells==0){
+                    hideTextView(1);
+                    hideTextView(2);
+                }
+                if(number_of_cells>0){
+                    hideTextView(2);
+                    CellInfoWcdma cellInfoWcdma1 = (CellInfoWcdma) cellInfoList.get(1);
+                    CellIdentityWcdma cellIdentityWcdma1 = cellInfoWcdma1.getCellIdentity();
+
+                    CellSignalStrengthWcdma cellSignalStrengthWcdma1 = cellInfoWcdma.getCellSignalStrength();
+                    int signalStrengthValue1= cellSignalStrengthWcdma1.getDbm();
+                    int asuValue = cellSignalStrengthWcdma1.getAsuLevel();
+
+                    Log.i("SILA 1",String.valueOf(signalStrengthValue1));
+
+                    rsrp_name1.setText("RSSI:");
+                    rsrq_name1.setText("");
+                    rssnr_name1.setText("");
+                    tac_name1.setText("LAC:");
+                    pci_name1.setText("PSC:");
+                    ci_name1.setText("CID:");
+
+                    rsrp_value1.setText(String.valueOf(signalStrengthValue1) + " dbm");
+                    asu_value1.setText(String.valueOf(asuValue));
+                    power_value1.setText(dbmToPower(signalStrengthValue1));
+                    rsrq_value1.setText("");
+                    rssnr_value1.setText("");
+
+                    tac_value1.setText(Integer.toString(cellIdentityWcdma1.getLac()));
+                    pci_value1.setText(Integer.toString(cellIdentityWcdma1.getPsc()));
+                    ci_value1.setText(Integer.toString(cellIdentityWcdma1.getCid()));
+                }
+                if(number_of_cells>1){
+                    CellInfoWcdma cellInfoWcdma2 = (CellInfoWcdma) cellInfoList.get(2);
+                    CellIdentityWcdma cellIdentityWcdma2 = cellInfoWcdma2.getCellIdentity();
+
+                    CellSignalStrengthWcdma cellSignalStrengthWcdma2 = cellInfoWcdma.getCellSignalStrength();
+                    int signalStrengthValue2= cellSignalStrengthWcdma2.getDbm();
+                    int asuValue2 = cellSignalStrengthWcdma2.getAsuLevel();
+
+                    Log.i("SILA 2",String.valueOf(signalStrengthValue2));
+
+                    rsrp_name2.setText("RSSI:");
+                    rsrq_name2.setText("");
+                    rssnr_name2.setText("");
+                    tac_name2.setText("LAC:");
+                    pci_name2.setText("PSC:");
+                    ci_name2.setText("CID:");
+
+                    rsrp_value2.setText(String.valueOf(signalStrengthValue2) + " dbm");
+                    asu_value2.setText(String.valueOf(asuValue2));
+                    power_value2.setText(dbmToPower(signalStrengthValue2));
+                    rsrq_value2.setText("");
+                    rssnr_value2.setText("");
+
+                    tac_value2.setText(Integer.toString(cellIdentityWcdma2.getLac()));
+                    pci_value2.setText(Integer.toString(cellIdentityWcdma2.getPsc()));
+                    ci_value2.setText(Integer.toString(cellIdentityWcdma2.getCid()));
+                }
+
             } else if (telephon_manager_1.getNetworkType() == TelephonyManager.NETWORK_TYPE_EDGE ||
                     telephon_manager_1.getNetworkType() == TelephonyManager.NETWORK_TYPE_GPRS) {
                 Log.i("TAG ", "EDGE/GPRS");
@@ -215,7 +329,7 @@ public class MainActivity extends Activity {
                     ci_name.setText("CID:");
 
                     rsrp_value.setText(String.valueOf(signalStrengthValue) + " dbm");
-                    asu_value.setText(String.valueOf(signalStrengthValue + 113));
+                    asu_value.setText(String.valueOf(cellSignalStrengthGsm.getAsuLevel()));
                     power_value.setText(dbmToPower(signalStrengthValue));
                     rsrq_value.setText("");
                     rssnr_value.setText("");
@@ -223,6 +337,58 @@ public class MainActivity extends Activity {
                     tac_value.setText(Integer.toString(cellIdentityGsm.getLac()));
                     pci_value.setText("");
                     ci_value.setText(Integer.toString(cellIdentityGsm.getCid()));
+
+                    int number_of_cells = telephon_manager_1.getNeighboringCellInfo().size();
+
+                    if(number_of_cells>1){
+                        CellInfoGsm  cellInfoGsm1 = (CellInfoGsm) cellInfoList.get(1);
+                        CellIdentityGsm cellIdentityGsm1 = cellInfoGsm1.getCellIdentity();
+
+                        CellSignalStrengthGsm signalStrenght1 =cellInfoGsm1.getCellSignalStrength();
+                        int signalStrengthValue1 = signalStrenght1.getDbm();
+
+                        rsrp_name1.setText("RSSI:");
+                        rsrq_name1.setText("");
+                        rssnr_name1.setText("");
+                        tac_name1.setText("LAC:");
+                        pci_name1.setText("");
+                        ci_name1.setText("CID:");
+
+                        rsrp_value1.setText(String.valueOf(signalStrengthValue1) + " dbm");
+                        asu_value1.setText(String.valueOf(signalStrengthValue + 113));
+                        power_value1.setText(dbmToPower(signalStrengthValue1));
+                        rsrq_value1.setText("");
+                        rssnr_value1.setText("");
+
+                        tac_value1.setText(Integer.toString(cellIdentityGsm1.getLac()));
+                        pci_value1.setText("");
+                        ci_value1.setText(Integer.toString(cellIdentityGsm1.getCid()));
+
+                    }
+                    if(number_of_cells>2){
+                        CellInfoGsm  cellInfoGsm2 = (CellInfoGsm) cellInfoList.get(2);
+                        CellIdentityGsm cellIdentityGsm2 = cellInfoGsm2.getCellIdentity();
+
+                        CellSignalStrengthGsm signalStrenght2 =cellInfoGsm2.getCellSignalStrength();
+                        int signalStrengthValue2 = signalStrenght2.getDbm();
+
+                        rsrp_name2.setText("RSSI:");
+                        rsrq_name2.setText("");
+                        rssnr_name2.setText("");
+                        tac_name2.setText("LAC:");
+                        pci_name2.setText("");
+                        ci_name2.setText("CID:");
+
+                        rsrp_value2.setText(String.valueOf(signalStrengthValue2) + " dbm");
+                        asu_value2.setText(String.valueOf(signalStrengthValue2 + 113));
+                        power_value2.setText(dbmToPower(signalStrengthValue2));
+                        rsrq_value2.setText("");
+                        rssnr_value2.setText("");
+
+                        tac_value2.setText(Integer.toString(cellIdentityGsm2.getLac()));
+                        pci_value2.setText("");
+                        ci_value2.setText(Integer.toString(cellIdentityGsm2.getCid()));
+                    }
                 }
             }
         }
@@ -281,6 +447,41 @@ public class MainActivity extends Activity {
                 }
             }
         }
+    }
+
+    private void hideTextView(int index){
+        if(index==1) {
+            rsrq_name1.setText("");
+            rssnr_name1.setText("");
+            rsrp_name1.setText("");
+            rsrp_value1.setText("");
+            rsrq_value1.setText("");
+            rssnr_value1.setText("");
+            asu_value1.setText("");
+            power_value1.setText("");
+            ci_value1.setText("");
+            pci_value1.setText("");
+            tac_value1.setText("");
+            pci_name1.setText("");
+            tac_name1.setText("");
+            ci_name1.setText("");
+        }else if(index==2){
+            rsrq_name2.setText("");
+            rssnr_name2.setText("");
+            rsrp_name2.setText("");
+            rsrp_value2.setText("");
+            rsrq_value2.setText("");
+            rssnr_value2.setText("");
+            asu_value2.setText("");
+            power_value2.setText("");
+            ci_value2.setText("");
+            pci_value2.setText("");
+            tac_value2.setText("");
+            pci_name2.setText("");
+            tac_name2.setText("");
+            ci_name2.setText("");
+        }
+
     }
 
     private String dbmToPower(int dbm){
